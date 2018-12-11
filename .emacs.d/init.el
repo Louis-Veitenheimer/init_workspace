bd9aa28b6138dbbf93ec2dd2bdf438134042e904
Louis Veitenheimer <25210190+Louis-Veitenheimer@users.noreply.github.com>
2018-12-10 22:09:27 -0600

    configured emacs to use spaces instead of tabs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,3 +13,4 @@
 (add-hook 'yaml-mode-hook #'ansible-doc-mode)
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
+(setq-default indent-tabs-mode nil) ; use spaces instead of tabs
