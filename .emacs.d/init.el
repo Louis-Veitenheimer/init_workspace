ec18dbabea9125a3c9482e586306d2501ccdcd13
Louis Veitenheimer <25210190+Louis-Veitenheimer@users.noreply.github.com>
2018-12-10 22:12:17 -0600

    added package-selected-packages var to emacs config
    This is a relatively new feature in emacs.  Each new package installed
    gets added to this list by emacs and running the command `M-x
    package-install-selected-packages`, emacs will install all packages in
    that var.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,3 +14,18 @@
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 (setq-default indent-tabs-mode nil) ; use spaces instead of tabs
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit docker-compose-mode dockerfile-mode alchemist ansible ansible-doc elixir-mode markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
